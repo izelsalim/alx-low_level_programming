d039cb5ba3668732a8a3b737ac116c0e2f33c700
salim <salim@gmail.com>
2023-03-08 18:35:45 -0800

    task

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,4 @@
-#include "main.h"
+nclude "main.h"
 /**
 * _strlen_recursion - Prints the length of a string.
 * @s: the string to be printed
@@ -6,6 +6,7 @@
 */
 int _strlen_recursion(char *s)
 {
+
 if (s[0] != '\0')
 return (1 + _strlen_recursion(s + 1));
 return (0);
@@ -32,7 +33,7 @@ return (0);
 * @s: base address for string.
 *
 * Return: 1 if n is prime, 0 otherwise.
-*/.
+*/
 int is_palindrome(char *s)
 {
 return (pal_checker(s, 0, _strlen_recursion(s) - 1));
