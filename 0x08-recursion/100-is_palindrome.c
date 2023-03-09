7dc8144f00b4883d83bdae61d273cd3e0758e213
salim <salim@gmail.com>
2023-03-08 18:43:06 -0800

    task

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -6,10 +6,9 @@ nclude "main.h"
 */
 int _strlen_recursion(char *s)
 {
-
-if (s[0] != '\0')
-return (1 + _strlen_recursion(s + 1));
-return (0);
+	if (s[0] != '\0')
+		return (1 + _strlen_recursion(s + 1));
+	return (0);
 }
 /**
 * pal_checker - check if s is palindrome.
@@ -20,13 +19,13 @@ return (0);
 */
 int pal_checker(char *s, int i, int j)
 {
-if (s[i] == s[j])
-if (i > j / 2)
-return (1);
-else
-return (pal_checker(s, i + 1, j - 1));
-else
-return (0);
+	if (s[i] == s[j])
+		if (i > j / 2)
+			return (1);
+		else
+			return (pal_checker(s, i + 1, j - 1));
+	else
+		return (0);
 }
 /**
 * is_palindrome - check if s is palindrome
@@ -36,5 +35,5 @@ return (0);
 */
 int is_palindrome(char *s)
 {
-return (pal_checker(s, 0, _strlen_recursion(s) - 1));
+	return (pal_checker(s, 0, _strlen_recursion(s) - 1));
 }
