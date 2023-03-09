93e457ce087abd98a8839f3ef2b70442beb7b9c9
salim <salim@gmail.com>
2023-03-08 18:59:40 -0800

    task

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -22,7 +22,6 @@
 * wildcmp - check if strings could be considered identical
 * @s1: base address for string.
 * @s2: base address for string.
-*
 * Return: 1 if are considered identical.
 */
 int wildcmp(char *s1, char *s2)
